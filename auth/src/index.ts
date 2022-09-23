96016e3f0fe51b858e03abd9a95aa0a8aba06c0b
David Roque <davidroque21@hotmail.com>
2022-09-23 14:32:43 -0400

    added mongoose and edit errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import "express-async-errors"
 import { json } from "body-parser"
+import mongoose from "mongoose"
 
 import { currentUserRouter, signInRouter, signOutRouter, signUpRouter } from "./routes"
 
 import { errorHandler } from "./middlewares"
-import { NotFoundError } from "./errors/not-found-error"
+import { NotFoundError } from "./errors"
 
 const app = express()
 app.use(json())
@@ -21,6 +22,16 @@ app.all("*", async (req, res, next) => {
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!!!!!!!!")
-})
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth")
+    console.log("Connected to MongoDB")
+  } catch (error) {
+    console.error(error)
+  }
+  app.listen(3000, () => {
+    console.log("Auth service running!")
+  })
+}
+
+start()
