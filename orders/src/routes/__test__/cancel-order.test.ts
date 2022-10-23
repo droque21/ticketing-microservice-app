29fe1db682af37679e53807d791f12df0529cc78
David Roque <davidroque21@hotmail.com>
2022-10-23 15:56:31 -0400

    complete order service missing nats event

---
+++ orders/src/routes/__test__/cancel-order.test.ts
@@ -0,0 +1,35 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Order, OrderStatus } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+describe('DELETE /api/orders/:orderId', () => {
+
+  it('marks an order as cancelled', async () => {
+    const cookie = global.signin();
+
+    const ticket = Ticket.build({
+      title: 'concert',
+      price: 20,
+    });
+
+    await ticket.save();
+
+    const { body: order } = await request(app)
+      .post('/api/orders')
+      .set('Cookie', cookie)
+      .send({ ticketId: ticket.id })
+      .expect(201);
+
+    await request(app)
+      .patch(`/api/orders/${order.id}`)
+      .set('Cookie', cookie)
+      .expect(200);
+
+    const updatedOrder = await Order.findById(order.id);
+
+    expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+  })
+
+  it.todo('emits a order cancelled event')
+})
---
+++ orders/src/routes/__test__/get-order.test.ts
@@ -0,0 +1,51 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+
+describe('GET /api/orders', () => {
+  it('fetch a order by id', async () => {
+    const cookie = global.signin();
+
+    const ticket = Ticket.build({
+      title: 'concert',
+      price: 20,
+    });
+
+    await ticket.save();
+
+    const { body: order } = await request(app)
+      .post('/api/orders')
+      .set('Cookie', cookie)
+      .send({ ticketId: ticket.id })
+      .expect(201);
+
+    const { body: fetchedOrder } = await request(app)
+      .get(`/api/orders/${order.id}`)
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(fetchedOrder.id).toEqual(order.id);
+  });
+
+  it('returns an error if one user tries to fetch another users order', async () => {
+    const cookie = global.signin();
+
+    const ticket = Ticket.build({
+      title: 'concert',
+      price: 20,
+    });
+
+    await ticket.save();
+
+    const { body: order } = await request(app)
+      .post('/api/orders')
+      .set('Cookie', cookie)
+      .send({ ticketId: ticket.id })
+      .expect(201);
+
+    await request(app)
+      .get(`/api/orders/${order.id}`)
+      .set('Cookie', global.signin())
+      .expect(401);
+  })
+});
